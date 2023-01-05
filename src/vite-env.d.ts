51e972a5eac88a50c8124fdfd5ed0317d27a424b
mtvzee <pgzv01@gmail.com>
2023-01-05 16:35:43 +0900

    unsplashのAPIキーの環境変数に対する型の追加

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_UNSPLASH_ACCESS_KEY: string
+  // その他の環境変数...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
