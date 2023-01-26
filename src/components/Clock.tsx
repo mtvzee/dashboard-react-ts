fea3f23ec61c658adae04f4ed8eb35e893eb15db
mtvzee <pgzv01@gmail.com>
2023-01-26 18:04:52 +0900

    文字の視認性を上げるために、文字サイズ、影の変更

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -12,11 +12,11 @@ const Clock = () => {
   }, []);
 
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-two">
-      <div className="absolute -top-4 h-[64px] w-full text-center leading-[64px] text-[2.5rem] tracking-wider">
+    <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ">
+      <div className="absolute -top-4 h-[64px] w-full text-center leading-[64px] text-[3rem] tracking-wider drop-shadow-[1px_1.5px_2px_rgba(0,0,0,0.7)] font-medium">
         {date.format('M月D日 dddd')}
       </div>
-      <div className="text-[11rem]">{date.format('HH:mm')}</div>
+      <div className="text-[12rem] drop-shadow-[1px_1.5px_3px_rgba(0,0,0,0.7)]">{date.format('HH:mm')}</div>
     </div>
   );
 };
