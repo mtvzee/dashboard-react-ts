13ba6d756e42ad1bc845e30dc5f6aa0592a3bd47
mtvzee <pgzv01@gmail.com>
2023-01-26 18:03:39 +0900

    dropShadowの削除

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,7 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      dropShadow: {
-        one: '1px 1px 1px rgba(0, 0, 0, 0.3)',
-        two: '2px 2px 2px rgba(0, 0, 0, 0.3)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
