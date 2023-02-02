b2ee2d9f07d4f66231cb6ce00f526572f604e3f3
mtvzee <pgzv01@gmail.com>
2023-02-02 18:38:55 +0900

    関数名とコメントの変更

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -17,13 +17,12 @@ const Todo = ({ id, text, isCompleted, todoList, setTodoList }: Props) => {
   const [isEditing, setIsEditing] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  // 編集ボタンを押した時にそのtodoにフォーカスする
+  // 編集ボタンを押した時にそのtodoのinputにフォーカスする
   useEffect(() => {
     inputRef.current?.focus();
   }, [isEditing]);
 
-  // todoを完了状態にする
-  const handleComplete = (id: number) => {
+  const handleToggleComplete = (id: number) => {
     setIsMoving(!isMoving);
     setTimeout(() => {
       setTodoList(
@@ -40,11 +39,11 @@ const Todo = ({ id, text, isCompleted, todoList, setTodoList }: Props) => {
     }, 1000);
   };
 
-  const handleDeleteBtn = (id: number) => {
+  const handleDeleteTodo = (id: number) => {
     setTodoList(todoList.filter((todo) => todo.id !== id));
   };
 
-  // todoを編集してエンターキーを押したときに、TODOリストの内容を変更する
+  // todoを編集してエンターキーを押したときに、Todoの内容を変更する
   const handleEnterKey = (
     id: number,
     e: React.KeyboardEvent<HTMLInputElement>
@@ -65,7 +64,7 @@ const Todo = ({ id, text, isCompleted, todoList, setTodoList }: Props) => {
     }
   };
   return (
-    <li className="flex items-center group" onClick={() => handleComplete(id)}>
+    <li className="flex items-center group" onClick={() => handleToggleComplete(id)}>
       <button className="mr-2">
         {isCompleted ? (
           isMoving ? (
@@ -92,7 +91,7 @@ const Todo = ({ id, text, isCompleted, todoList, setTodoList }: Props) => {
         className="invisible opacity-0 transition duration-300  group-hover:visible group-hover:opacity-100 text-[#eeeeee]"
         onClick={(e) => e.stopPropagation()}
       >
-        <button onClick={() => handleDeleteBtn(id)}>
+        <button onClick={() => handleDeleteTodo(id)}>
           <AiOutlineDelete className="w-6 h-6 hover:text-white" />
         </button>
         <button onClick={() => setIsEditing(!isEditing)}>
